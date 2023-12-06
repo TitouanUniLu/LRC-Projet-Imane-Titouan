20e59dcf67e3c704f91fb9774f4c6c9ecf305a16
Titouan <titouanguerin@gmail.com>
2023-12-06 13:25:41 +0100

    fin partie1 + tests

--- main.pl
+++ main.pl
@@ -44,8 +44,47 @@ verifier_auto_reference() :-
     (pas_autoref(Tbox), write('This Tbox is not auto-referent.'), nl,!
     ;   write('Erreur : auto-reference detected '),fail), nl. 
 
+%remplacement récursif
+remplace(C, C) :- 
+    cnamea(C), !. % Vérifie si C est un concept atomique
+ 
+
+remplace(C, E) :- % Si C est équivalent à un autre concept D on récupère l'équivalent D de C et remplacer par son équivalent 
+    equiv(C, D), remplace(D, E), !. 
+
+remplace(not(C1), not(C2)) :- % Si C1 est une négation remplacer par la négation
+    remplace(C1, C2), !. 
+
+remplace(or(C1, C2), or(D1, D2)) :- % Si C disjonction (or), remplacer par première disjonction puis deuxième disjonction
+    remplace(C1, D1), remplace(C2, D2), !. 
+
+remplace(and(C1, C2), and(D1, D2)) :- % pareil pour and
+    remplace(C1, D1), remplace(C2, D2), !. 
+
+remplace(some(R, C1), some(R, C2)) :- % meme logique
+    remplace(C1, C2), !. 
+
+remplace(all(R, C1), all(R, C2)) :- 
+    remplace(C1, C2), 
+    !. 
 
 %traitement TBox  T
+traitement_Tbox([],[]).
+traitement_Tbox([(C1,C2)|O],[(C1,C3)|O1]) :- remplace(C2,C4), nnf(C4,C3), traitement_Tbox(O,O1), !. %call recursif pour le reste
 
 %traitement ABox  T
+traitement_Abox([],[]).
+traitement_Abox([(I,C)|O],[(I,C1)|O1]) :- remplace(C,C2), nnf(C2,C1), traitement_Abox(O,O1), !.
+traitement_Abox([(I1,I2,R)|O],[(I1,I2,R)|O1]) :- traitement_Abox(O,O1), !.
 
+% TESTS POUR VOIR SI CA MARCHE ON PEUT ENLEVER APRES%
+test1 :-
+    traitement_Tbox([(sculpteur, some(aCree, sculpture)), (auteur, some(aEcrit, livre))], ResultatTbox),
+    writeln('Resultat du traitement de la TBox :'),
+    writeln(ResultatTbox).
+
+test2 :-  
+    traitement_Tbox([(sculpteur, some(aCree, sculpture)), (auteur, some(aEcrit, livre))], ResultatTbox),
+    traitement_Abox([(michelAnge, personne), (david, sculpture), (sonnets, livre)], ResultatAbox),
+    writeln('Resultat du traitement de la TBox :'), writeln(ResultatTbox),
+    writeln('Resultat du traitement de la ABox :'), writeln(ResultatAbox).
