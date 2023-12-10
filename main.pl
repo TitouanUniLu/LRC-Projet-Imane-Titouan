795cb8e6bc5817e2d427b34768858e0e5b32b6c3
HDBImane <hadbiimane2@gmail.com>
2023-12-10 20:06:01 +0100

    Changes to be committed: 	modified:   main.pl

--- main.pl
+++ main.pl
@@ -13,6 +13,13 @@ nnf(some(R,C),some(R,NC)):- nnf(C,NC),!.
 nnf(all(R,C),all(R,NC)) :- nnf(C,NC),!.
 nnf(X,X).
 
+listTBox(Tbox) :- setof((C, E), equiv(C, E), Tbox).
+listAboxInst(AboxInst):- setof((I1, I2), inst(I1, I2), AboxInst).
+listAboxRole(AboxRole):- setof((I1, I2, R), instR(I1, I2, R), AboxRole).
+
+
+%Implémentation de la partie 1 
+
 %concepts
 concept(and(C1,C2)) :- concept(C1), concept(C2).
 concept(or(C1,C2)) :- concept(C1) , concept(C2).
@@ -21,16 +28,24 @@ concept(all(R, C1)) :- rname(R), concept(C1).
 concept(some(R, C1)) :- rname(R), concept(C1).
 concept(C1) :- cnamea(C1).
 concept(C1) :- cnamena(C1).
-concept(C1) :- iname(C1).
-concept(C1) :- rname(C1).
+concept(C,E) :- cnamena(C), concept(E).
 
-verifier_concept() :-
-    setof(E, equiv(_, E), Concepts),
-    member(C, Concepts),
-    \+ concept(C),
-    write('Invalid concept: '), write(C), nl,!, fail. % La fonction s'arrête dès qu'elle trouve un concept invalide
-verifier_concept :- 
-    write('Verification: Syntax and semantics are correct.'), nl.
+concept(I,C):- iname(I), cnamea(C).
+concept(I,C):- iname(I), cnamena(C).
+concept(I1, I2, R):- iname(I1), iname(I2), rname(R).
+
+verifier_concept(Tbox, AboxInst, AboxRole) :-
+    listTBox(Tbox),
+    listAboxInst(AboxInst),
+    listAboxRole(AboxRole),
+    (member((C,E), Tbox), \+ concept(C,E), 
+     write('Concept invalide dans : '), write((C,E)), nl, !, fail
+    ; member((I,C), AboxInst), \+ concept(I,C), 
+      write('Concept invalide dans : '), write((I,C)), nl, !, fail
+    ; member((I1,I2,R), AboxRole), \+ concept(I1,I2,R), 
+      write('Concept invalide dans : '), write((I1,I2,R)), nl, !, fail
+    ; true), 
+    write("Verification : Syntaxe et semantique correctes"), nl.
 
 %autoref 
 autoref(C, C) :- cnamea(C), !.
@@ -48,10 +63,10 @@ pas_autoref([(C, E)|R]) :-
 pas_autoref([]).
 
 
-verifier_auto_reference() :- 
-    setof((C, E), equiv(C, E), Tbox), 
-    (pas_autoref(Tbox), write('This Tbox is not auto-referent.'), nl,!
-    ;   write('Erreur : auto-reference detected '),fail), nl.
+verifier_auto_reference(Tbox) :- 
+    listTBox(Tbox),
+    (pas_autoref(Tbox), write("Cette T-Box n\'est pas auto-referente."), nl,!
+    ;   write("Erreur : Auto-reference detectee. "),fail), nl.
 % Si aucune auto-référence n'est détectée, elle affiche un message indiquant que la T-Box n'est pas auto-référente.
 % Sinon, elle affiche un message d'erreur indiquant que l'auto-référence a été détectée et provoque un échec. 
 
@@ -88,27 +103,64 @@ traitement_Abox([],[]).
 traitement_Abox([(I,C)|O],[(I,C1)|O1]) :- remplace(C,C2), nnf(C2,C1), traitement_Abox(O,O1), !.
 traitement_Abox([(I1,I2,R)|O],[(I1,I2,R)|O1]) :- traitement_Abox(O,O1), !.
 
-% TESTS POUR VOIR SI CA MARCHE ON PEUT ENLEVER APRES%
-test1 :-
-    traitement_Tbox([(sculpteur, some(aCree, sculpture)), (auteur, some(aEcrit, livre))], ResultatTbox),
-    writeln('Resultat du traitement de la TBox :'),
-    writeln(ResultatTbox).
 
-test2 :-  
-    traitement_Tbox([(sculpteur, some(aCree, sculpture)), (auteur, some(aEcrit, livre))], ResultatTbox),
-    traitement_Abox([(michelAnge, personne), (david, sculpture), (sonnets, livre)], ResultatAbox),
-    writeln('Resultat du traitement de la TBox :'), writeln(ResultatTbox),
-    writeln('Resultat du traitement de la ABox :'), writeln(ResultatAbox).
-
-traitement_Tbox_Abox() :- 
-    setof((C, E), equiv(C, E), Tbox), 
-    setof((I, C), inst(I, C), AboxC),
-    setof((I1, I2, R), instR(I1, I2, R), AboxR),
+traitement_Tbox_Abox(Tbox, AboxInst, AboxRole) :-
+    listTBox(Tbox),
+    listAboxInst(AboxInst),
+    listAboxRole(AboxRole),
     traitement_Tbox(Tbox, ResultatTbox),
-    traitement_Abox(AboxC, ResultatAboxC),
-    traitement_Abox(AboxR, ResultatAboxR),
+    traitement_Abox(AboxInst, ResultatAboxInst),
+    traitement_Abox(AboxRole, ResultatAboxRole),
     writeln('Resultat du traitement de la TBox :'), writeln(ResultatTbox),
-    writeln('Resultat du traitement de la ABox :'), writeln(ResultatAboxC), writeln(ResultatAboxR).
+    writeln('Resultat du traitement de la ABox :'), writeln(ResultatAboxInst), writeln(ResultatAboxRole).
 
 % exécute tous les vérifications et traitements
-partie1 :- verifier_concept(), verifier_auto_reference(), traitement_Tbox_Abox().
+premiere_etape(Tbox, Abi, Abr) :- verifier_concept(Tbox, Abi, Abr), verifier_auto_reference(Tbox), traitement_Tbox_Abox(Tbox, Abi, Abr).
+
+%Implémentation de la partie 2 
+
+programme :-
+    premiere_etape(Tbox, Abi, Abr),
+    deuxieme_etape(Abi, Abi1, Tbox),
+    troisieme_etape(Abi1, Abr).
+
+deuxieme_etape(Abi, Abi1, Tbox) :-
+    saisie_et_traitement_prop_a_demontrer(Abi, Abi1, Tbox).
+
+saisie_et_traitement_prop_a_demontrer(Abi, Abi1, Tbox) :-
+    nl, write('Entrez le numéro du type de proposition que vous voulez démontrer :'), nl,
+    write('1. Une instance donnée appartient à un concept donné.'), nl,
+    write('2. Deux concepts n"ont pas d"éléments en commun (ils ont une intersection vide).'), nl,
+    read(R), suite(R, Abi, Abi1, Tbox).
+
+suite(1, Abi, Abi1, Tbox) :-
+    acquisition_prop_type1(Abi, Abi1, Tbox), !.
+suite(2, Abi, Abi1, Tbox) :-
+    acquisition_prop_type2(Abi, Abi1, Tbox), !.
+suite(_, Abi, Abi1, Tbox) :-
+    nl, write('Cette réponse est incorrecte.'), nl,
+    saisie_et_traitement_prop_a_demontrer(Abi, Abi1, Tbox).
+
+entrerInstance(I) :-
+    nl, write("Entrez l\'identifiant de l\'instance (I) : "), nl,
+    read(I),
+    iname(I) -> true ; 
+        write("Erreur : Instance invalide. Veuillez entrer une instance valide."), nl,
+        entrerInstance(I).
+
+entrerConcept(C) :-
+    nl, write("Entrez l\'expression du concept (C) : "), nl,
+    read(C),
+    concept(C) -> true ; 
+        write("Erreur : Concept invalide. Veuillez entrer un concept valide."), nl,
+        entrerConcept(C).
+
+acquisition_prop_type1(_, [(I,C1)|_], _) :-
+    entrerInstance(I),
+    entrerConcept(C),
+    remplace(not(C), NC), nnf(NC, C1),
+    nl, write("Proposition ajoutee avec succes : "), write(inst(I, C1)), nl.
+
+
+% acquisition_prop_type2(Abi, Abi1, Tbox) :-
+
